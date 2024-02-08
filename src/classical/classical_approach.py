14b26a3b6a3b093c0ffb0fed861e570ab75ca70e
Daniel-Packer <packer.61@osu.edu>
2024-02-08 12:58:13 -0500

    added basic classical approach

--- src/classical/classical_approach.py
+++ src/classical/classical_approach.py
@@ -1,6 +1,13 @@
 import pickle
 from pathlib import Path
+from matplotlib import pyplot as plt
+from jax import numpy as jnp
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 root_path = Path().resolve().parents[1]
-data_path = root_path / 'data'
-halos_path = data_path / 'halos.pkl'
+data_path = root_path / "data"
+halos_path = data_path / "halos.pkl"
+
+G = 4.30219372e10 / 1e6
