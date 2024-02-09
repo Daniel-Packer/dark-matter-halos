d4a6fdab86a7facc92e5577738b82feeadee3904
Daniel-Packer <packer.61@osu.edu>
2024-02-09 14:01:15 -0500

    added linaer classical model

--- src/classical/classical_approach.py
+++ src/classical/classical_approach.py
@@ -1,10 +1,15 @@
 import pickle
 from pathlib import Path
 from matplotlib import pyplot as plt
-from jax import numpy as jnp
+from jax import numpy as jnp, random
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from numpyro.infer import MCMC, NUTS, Predictive
+from numpyro import sample, distributions as dist
+import arviz as az
 
+
+rng = random.PRNGKey(0)
 
 root_path = Path().resolve().parents[1]
 data_path = root_path / "data"
