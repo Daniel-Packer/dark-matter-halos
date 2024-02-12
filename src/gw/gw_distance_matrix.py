e3d4c078eed044d3828c1161de9135e513bee9c3
Daniel-Packer <packer.61@osu.edu>
2024-02-12 13:33:01 -0500

    started_mini_gw_example

---
+++ src/gw/gw_distance_matrix.py
@@ -0,0 +1,6 @@
+from ott.solvers.quadratic import gromov_wasserstein
+from ott.problems.quadratic import quadratic_problem
+from ott.solvers.quadratic import lower_bound
+from ott.geometry import pointcloud
+from pathlib import Path
+import pickle
